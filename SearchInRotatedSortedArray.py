7d7993e7fdfa4579e7a814b69d2855ede0b569ee
Hsuan-Hau (Howard) Liu <23202168+hsuanhauliu@users.noreply.github.com>
2018-05-17 02:29:11 -0700

    optimized code and deleted debugging prints

--- SearchInRotatedSortedArray.py
+++ SearchInRotatedSortedArray.py
@@ -10,39 +10,29 @@ class Solution(object):
             return -1
 
         if len(nums) == 1:
-            #print "case 0"
             if nums[0] == target:
                 return 0
             else:
                 return -1
         
         # determine range
-        #print "case 1"
         mid = len(nums) / 2
-
         start_num = nums[0]
-        end_num = nums[mid]
+        mid_num = nums[mid]
 
-        if start_num < end_num:
-            if start_num <= target and target < end_num:
-                temp = self.search(nums[:mid], target)
-                if temp == -1:
-                    return -1
-                return temp
+        if start_num < mid_num:
+            if start_num <= target and target < mid_num:
+                return self.search(nums[:mid], target)
 
-            #print "case 2"
             temp = self.search(nums[mid:], target)
             if temp == -1:
                 return -1
             return mid + temp
 
-        if end_num <= target and target < start_num:
+        if mid_num <= target and target < start_num:
             temp = self.search(nums[mid:], target)
             if temp == -1:
                 return -1
             return mid + temp
 
-        temp = self.search(nums[:mid], target)
-        if temp == -1:
-            return -1
-        return temp
+        return self.search(nums[:mid], target)
